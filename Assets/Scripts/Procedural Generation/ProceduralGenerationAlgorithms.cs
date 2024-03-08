3216ca6eab2c39724cfc5f34d1eea24904f2772e
MakaveliDevv <dev.makavelii@gmail.com>
2024-03-08 18:10:46 +0100

    Update 2.2
    The start of Procedural Dungeon Generation

---
+++ Assets/Scripts/Procedural Generation/ProceduralGenerationAlgorithms.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ProceduralGenerationAlgorithms
+{
+    public static HashSet<Vector2Int> SimpleRandomWalk(Vector2Int startPosition, int walkLength) 
+    {
+        HashSet<Vector2Int> path = new()
+        {
+            startPosition
+        };
+        
+        var previousPosition = startPosition;
+
+        for (int i = 0; i < walkLength; i++)
+        {
+            var newPosition = previousPosition + Direction2D.GetRandomCardinalDirection();
+            path.Add(newPosition);
+
+            previousPosition = newPosition;
+        }
+
+        return path;
+    }
+}
+
+public static class Direction2D
+{
+    public static List<Vector2Int> cardinalDirectionsList = new()
+    {
+        new Vector2Int(0, 1), // UP 
+        new Vector2Int(0, -1), // DOWN
+        new Vector2Int(1, 0), // RIGHT
+        new Vector2Int(-1, 0), // LEFT
+    };
+
+    public static Vector2Int GetRandomCardinalDirection() 
+    {
+        return cardinalDirectionsList[Random.Range(0, cardinalDirectionsList.Count)];
+    }
+}
